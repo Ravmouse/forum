c810cfa6238fea7d39f5e59aa5d74d5e590b9135
Vitaly <rav.energ@rambler.ru>
2022-04-01 15:34:48 +0300

    Deleted unnecessary getters and setters as I added lombok

--- src/main/java/ru/vitaly/entity/Post.java
+++ src/main/java/ru/vitaly/entity/Post.java
@@ -1,6 +1,9 @@
 package ru.vitaly.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,11 +15,12 @@ import javax.persistence.Table;
 import java.time.LocalDate;
 
 /**
- * @author Vitaly Vasilyev, date: 02.06.2020, e-mail: rav.energ@rambler.ru
- * @version 1.0
+ * @author Vitaly Vasilyev, e-mail: rav.energ@rambler.ru
  */
 @Entity
 @Table(name = "posts")
+@Data
+@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,73 +33,6 @@ public class Post {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    private String image_name;
-
-    public Post() { }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getCreated() {
-        return created;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getImage_name() {
-        return image_name;
-    }
-
-    public void setImage_name(String image_name) {
-        this.image_name = image_name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof Post)) {
-            return false;
-        }
-        Post post = (Post) o;
-        return id != 0 && id == post.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return 31;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("id=%d, name=%s, description=%s, created=%s, image_name=%s", id, name, description, created, image_name);
-    }
+    @Column(name = "image_name")
+    private String imgName;
 }
