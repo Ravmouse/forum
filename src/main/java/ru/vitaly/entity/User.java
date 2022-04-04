dfb04e61ed3dd7f3306524c645d18617e9ce0f09
Vitaly Vasilyev <rav.energ@rambler.ru>
2022-04-04 23:36:37 +0300

    Deleted getters and setters from User class

--- src/main/java/ru/vitaly/entity/User.java
+++ src/main/java/ru/vitaly/entity/User.java
@@ -1,5 +1,7 @@
 package ru.vitaly.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,11 +10,12 @@ import javax.persistence.Table;
 import java.time.LocalDate;
 
 /**
- * @author Vitaly Vasilyev, date: 03.06.2020, e-mail: rav.energ@rambler.ru
- * @version 1.0
+ * @author Vitaly Vasilyev, e-mail: rav.energ@rambler.ru
  */
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,8 +27,6 @@ public class User {
     private String login;
     private String password;
 
-    public User() { }
-
     public User(String name, String surname, LocalDate birth, String email, String login, String password) {
         this.name = name;
         this.surname = surname;
@@ -33,76 +34,5 @@ public class User {
         this.email = email;
         this.login = login;
         this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public LocalDate getBirth() {
-        return birth;
-    }
-
-    public void setBirth(LocalDate birth) {
-        this.birth = birth;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof User)) {
-            return false;
-        }
-        User post = (User) o;
-        return id != 0 && id == post.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return 33;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("id=%d, name=%s, surname=%s, birth=%s, email=%s, login=%s, password=%s"
-                , id, name, surname, birth, email, login, password);
     }
 }
