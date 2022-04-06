019601f7644feb49a4c6c7b2cbdf4122c403b2b7
Vitaly Vasilyev <rav.energ@rambler.ru>
2022-04-06 23:33:01 +0300

    Added @Query annotation & fetch join

--- src/main/java/ru/vitaly/repository/PostRepository.java
+++ src/main/java/ru/vitaly/repository/PostRepository.java
@@ -1,12 +1,14 @@
 package ru.vitaly.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.vitaly.entity.Post;
+import java.util.List;
 
 /**
- * @author Vitaly Vasilyev, date: 03.06.2020, e-mail: rav.energ@rambler.ru
- * @version 1.0
+ * @author Vitaly Vasilyev, e-mail: rav.energ@rambler.ru
  */
 public interface PostRepository extends CrudRepository<Post, Integer> {
-
+    @Query("select p from Post p join fetch p.user u")
+    List<Post> findAllPosts();
 }
